a13e2094887b2dde4cbcd7cf8828ff68e4d3fb88
Gio Mungcal <gcmungcal@gmail.com>
2024-09-01 17:51:23 +0800

    Removed search bar ffrom profile page.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -11,7 +11,7 @@ import React from "react";
 function page() {
   return (
     <>
-      <Header />
+      <Header isSearchBarHidden={true} />
       <UserDetails />
       <hr />
       <SavedPins />
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -5,13 +5,17 @@ import SearchBar from "../common/SearchBar";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
 
-function Header() {
+function Header({ isSearchBarHidden }) {
   return (
     <header className="h-[80px] w-full py-1 px-4 flex items-center">
       <div className="h-[56px] w-full flex justify-evenly items-center">
         <Logo isClickable={true} isRed={true} />
         <Navigation />
-        <SearchBar placeholderText={"denim, hairstyles.."} />
+        {!isSearchBarHidden ? (
+          <SearchBar placeholderText={"denim, hairstyles.."} />
+        ) : (
+          <div className="w-[70%]"></div>
+        )}
         <Button icon={"notificationIcon"} displayWhenSmallScreen={false} />
         <Button icon={"chatbubbleIcon"} displayWhenSmallScreen={false} />
         <Button
